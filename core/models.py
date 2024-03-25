94dd010efb5dd033260a813fd66010c5eab8a604
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-26 01:51:00 +0330

    :hammer: chore: create custom AuditManager

--- core/models.py
+++ core/models.py
@@ -7,7 +7,24 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
 
+class AuditableModelManager(models.Manager):
+    def create(self, user=None, **kwargs):
+        if user is not None:
+            kwargs.setdefault('created_by', user)
+        return super().create(**kwargs)
+
+    def update(self, user=None, **kwargs):
+        if user is not None:
+            kwargs['updated_by'] = user
+        return super().update(**kwargs)
+
+
 class AuditableModel(models.Model):
+    class Meta:
+        abstract = True
+
+    objects = AuditableModelManager()
+
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
 
@@ -16,9 +33,6 @@ class AuditableModel(models.Model):
 
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
-
-    class Meta:
-        abstract = True
 
     def save(self, user=None, *args, **kwargs):
         if user is not None:
