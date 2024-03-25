e3ae0d16ae2d48b11bebd26b223590e867126c1d
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-25 17:43:49 +0330

    :heavy_plus_sign: conf: connect to mysql

--- holding/settings.py
+++ holding/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'holding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'holding',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3030',
     }
 }
 
