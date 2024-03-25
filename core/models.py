a53fb16f8d0b28b5b3ba3d77660bca9ff7c14028
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-26 02:00:35 +0330

    :poop: hammer: remove some dummy code

--- core/models.py
+++ core/models.py
@@ -7,23 +7,9 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
 
-class AuditableModelManager(models.Manager):
-    def create(self, user=None, **kwargs):
-        if user is not None:
-            kwargs.setdefault('created_by', user)
-        return super().create(**kwargs)
-
-    def update(self, user=None, **kwargs):
-        if user is not None:
-            kwargs['updated_by'] = user
-        return super().update(**kwargs)
-
-
 class AuditableModel(models.Model):
     class Meta:
         abstract = True
-
-    objects = AuditableModelManager()
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
@@ -33,10 +19,3 @@ class AuditableModel(models.Model):
 
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
-
-    def save(self, user=None, *args, **kwargs):
-        if user is not None:
-            if not self.pk:
-                self.created_by = user
-            self.updated_by = user
-        super().save(*args, **kwargs)
