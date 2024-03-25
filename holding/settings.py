2c578d5ea9793a14ab4e76e9ba5b628f31b4d983
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-25 17:26:15 +0330

    :heavy_plus_sign: conf: install django debug toolbar

--- holding/settings.py
+++ holding/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'holding.urls'
