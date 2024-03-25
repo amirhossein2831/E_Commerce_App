501ea84d696a7b36b9f894bb4ab2bec05126d51a
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-25 20:32:09 +0330

    :iphone: feat: create AuditableModel

--- core/models.py
+++ core/models.py
@@ -2,7 +2,27 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class User(AbstractUser):
     # add extra field here
     email = models.EmailField(unique=True)
+
+
+class AuditableModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, primary_key=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, user=None, *args, **kwargs):
+        if user is not None:
+            if not self.pk:
+                self.created_by = user
+            self.updated_by = user
+        super().save(*args, **kwargs)
