cdc3ec95e233d7e77c44437ccbe3079efa2c8c61
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 21:49:54 +0330

    :hammer: chore: change the AuditableModel to commonModel

--- common/models.py
+++ common/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class AuditableModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
+
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
