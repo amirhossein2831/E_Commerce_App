8186ea5f7b1872d2ca5a7e2456773f44a17d30cd
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 01:08:58 +0330

    :hammer: chore: add permisstion to CustomerViewSet

--- store/views.py
+++ store/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.decorators import action
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from store.serializers import CustomerSerializer
 from store.models import Customer
+from store.serializers import CustomerSerializer
 
 
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
