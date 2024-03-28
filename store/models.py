c4b4fee77a5314b63fb502471ec909457d9ef019
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 21:52:35 +0330

    :truck: chore: remove the common app and move Auditable to store

--- store/models.py
+++ store/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.conf import settings
-from common.models import AuditableModel
 
+
+class AuditableModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
+
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="+")
 
 
 class Customer(AuditableModel):
