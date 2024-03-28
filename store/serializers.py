b631639f586ebaaef20d00858d9e50e4b0fb19d5
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 00:31:42 +0330

    :hammer: chore: create customer serializer and get logged in customer

--- store/serializers.py
+++ store/serializers.py
@@ -4,8 +4,9 @@ from store.models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    user_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Customer
-        fields = ['user', 'phone', 'birth_date', 'membership']
+        fields = ['user_id', 'phone', 'birth_date', 'membership']
+
--- store/views.py
+++ store/views.py
@@ -1,4 +1,9 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from store.serializers import CustomerSerializer
 from store.models import Customer
@@ -9,3 +14,17 @@ from store.models import Customer
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request: Request) -> Response:
+        customer = get_object_or_404(Customer, user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
