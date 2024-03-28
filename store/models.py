1eab8507afacbdb160af13083ac0fcc0618bad2c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 02:01:38 +0330

    :hammer: chore: create Order and order item class

--- store/models.py
+++ store/models.py
@@ -85,3 +85,26 @@ class Address(AuditableModel):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     zip_code = models.CharField(max_length=5, default='-')
+
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(AuditableModel):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
