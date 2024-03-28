3f6dd1fb9cf808c800a10e1ee98b5f99926c2c84
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 21:09:21 +0330

    :sparkles: feat: create Cutomer model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Customer(settings.BASE_MODEL):
+    BRONZE_MEMBERSHIP = 'B'
+    SILVER_MEMBERSHIP = 'S'
+    GOLD_MEMBERSHIP = 'G'
+
+    MEMBERSHIP_PLAN = [
+        (BRONZE_MEMBERSHIP, 'BRONZE MEMBERSHIP'),
+        (SILVER_MEMBERSHIP, 'SILVER MEMBERSHIP'),
+        (GOLD_MEMBERSHIP, 'GOLD MEMBERSHIP'),
+    ]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    phone = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_PLAN, default=BRONZE_MEMBERSHIP)
