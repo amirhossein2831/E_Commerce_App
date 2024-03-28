53edf27320f39a1d10b09910e16a23e0ec2a774a
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 02:35:39 +0330

    :hammer: chore: create view set for collection

--- store/views.py
+++ store/views.py
@@ -6,13 +6,13 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from store.models import Customer
-from store.serializers import CustomerSerializer
+from store.models import Customer, Collection
+from store import serializers
 
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+    serializer_class = serializers.CustomerSerializer
     permission_classes = [IsAdminUser]
 
     def perform_create(self, serializer):
@@ -22,9 +22,15 @@ class CustomerViewSet(ModelViewSet):
     def me(self, request: Request) -> Response:
         customer = get_object_or_404(Customer, user=request.user)
         if request.method == 'GET':
-            serializer = CustomerSerializer(customer)
+            serializer = serializers.CustomerSerializer(customer)
             return Response(serializer.data, status=status.HTTP_200_OK)
         elif request.method == 'PUT':
-            serializer = CustomerSerializer(customer, request.data)
+            serializer = serializers.CustomerSerializer(customer, request.data)
             serializer.is_valid(raise_exception=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = serializers.CollectionSerializer
+
