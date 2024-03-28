527c02b75f6e607c8b5646848391d56d65eaffeb
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 23:34:46 +0330

    :sparkles: feat: create route and send api for customer

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
+from store.serializers import CustomerSerializer
+from store.models import Customer
+
 
 # Create your views here.
+
+class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
