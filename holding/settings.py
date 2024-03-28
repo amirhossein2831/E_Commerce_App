5160ce9730d4ac8dc54c3c2e4cc2d298c9cf2d1f
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 19:31:20 +0330

    :wrench: conf: add Default filter and pagination

--- holding/settings.py
+++ holding/settings.py
@@ -143,6 +143,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 SIMPLE_JWT = {
