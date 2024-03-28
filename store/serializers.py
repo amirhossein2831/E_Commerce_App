7758c0569f9103beb1c5509490e344eaed8693f5
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 00:52:43 +0330

    :hammer: chore: set the logged user id to the user_id of the customer

--- store/serializers.py
+++ store/serializers.py
@@ -4,9 +4,9 @@ from store.models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Customer
-        fields = ['user_id', 'phone', 'birth_date', 'membership']
+        fields = ['user', 'phone', 'birth_date', 'membership']
 
--- store/views.py
+++ store/views.py
@@ -9,22 +9,20 @@ from store.serializers import CustomerSerializer
 from store.models import Customer
 
 
-# Create your views here.
-
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request: Request) -> Response:
-        customer = get_object_or_404(Customer, user_id=request.user.id)
+        customer = get_object_or_404(Customer, user=request.user)
         if request.method == 'GET':
             serializer = CustomerSerializer(customer)
             return Response(serializer.data, status=status.HTTP_200_OK)
         elif request.method == 'PUT':
             serializer = CustomerSerializer(customer, request.data)
             serializer.is_valid(raise_exception=True)
-            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-
