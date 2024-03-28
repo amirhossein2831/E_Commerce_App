ab1aa46286dc965d9ef21186d33755fcf89e3c28
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 01:37:21 +0330

    :hammer: chore: create Can View  History Permisstion

--- store/permissoin.py
+++ store/permissoin.py
@@ -6,3 +6,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
+
+
+class CanViewHistory(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('store.view_history')
