756791eaa0d8d549a038fedcbfea66fe8cad2121
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 21:33:59 +0330

    :hamemr: chore: create nested route for product promotions

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
 from . import views
 
-route = DefaultRouter()
+route = routers.DefaultRouter()
 route.register('customers', views.CustomerViewSet)
 route.register('collections', views.CollectionViewSet)
 route.register('products', views.ProductViewSet)
 route.register('promotions', views.PromotionViewSet)
 
-urlpatterns = route.urls
+promotions_router = routers.NestedDefaultRouter(route, 'products', lookup='products')
+promotions_router.register('promotions', views.ProductPromotionViewSet, basename='promotions')
+
+urlpatterns = route.urls + promotions_router.urls
