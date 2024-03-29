0c6faf788c1cb5f0b96c120c3dc16e188d78357e
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 21:06:02 +0330

    :hammer: chore: add Promotion route

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ route = DefaultRouter()
 route.register('customers', views.CustomerViewSet)
 route.register('collections', views.CollectionViewSet)
 route.register('products', views.ProductViewSet)
+route.register('promotions', views.PromotionViewSet)
 
 urlpatterns = route.urls
