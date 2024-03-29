c45975aebeb04ad5c618d884e184b41f4ade130c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-29 19:25:40 +0330

    :hammer: chore: add some condition to relation validation

--- store/validator.py
+++ store/validator.py
@@ -1,12 +1,13 @@
 def has_relation(model, serializers, value, many=False):
-    if many:
-        if value is not None:
-            for instance_id in value:
-                if not model.objects.filter(pk=instance_id).exists():
-                    raise serializers.ValidationError(f'{model.__name__} with ID {instance_id} does not exist.')
+    if value is not None and not value:
+        return None
+    if value is None:
         return value
 
-    if value is not None:
+    if many:
+        for instance_id in value:
+            if not model.objects.filter(pk=instance_id).exists():
+                raise serializers.ValidationError(f'{model.__name__} with ID {instance_id} does not exist.')
+    else:
         if not model.objects.filter(pk=value).exists():
             raise serializers.ValidationError(f'{model.__name__} with ID {value} does not exist.')
-    return value
