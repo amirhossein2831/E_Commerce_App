374d9b067034f20f62eafd13dcb36d113ed55dea
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-30 20:13:35 +0330

    :hammer: chore: add nested route for the cart_items

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,7 @@ route.register('customers', views.CustomerViewSet)
 route.register('collections', views.CollectionViewSet)
 route.register('products', views.ProductViewSet)
 route.register('promotions', views.PromotionViewSet)
-route.register('cart', views.CartViewSet)
+route.register('carts', views.CartViewSet)
 route.register('customers/me/addresses', views.LoginCustomerAddressViewSet)
 
 promotions_router = routers.NestedDefaultRouter(route, 'products', lookup='products')
@@ -19,5 +19,7 @@ reviews_router.register('reviews', views.ProductReviewViewSet, basename='reviews
 addresses_router = routers.NestedDefaultRouter(route, 'customers', lookup='customers')
 addresses_router.register('addresses', views.CustomerAddressViewSet, basename='addresses')
 
+cart_items_router = routers.NestedDefaultRouter(route, 'carts', lookup='carts')
+cart_items_router.register('items', views.CartItemViewSet, 'items')
 
-urlpatterns = route.urls + promotions_router.urls + reviews_router.urls + addresses_router.urls
+urlpatterns = route.urls + promotions_router.urls + reviews_router.urls + addresses_router.urls + cart_items_router.urls
