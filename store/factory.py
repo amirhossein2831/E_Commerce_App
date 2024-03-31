59cd0447132e74ed1d84be73940ec9ae243d4e27
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-01 00:11:20 +0330

    :hammer: chore: create userwith the profile

--- store/factory.py
+++ store/factory.py
@@ -8,28 +8,22 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = settings.AUTH_USER_MODEL
 
-    username = factory.Sequence(lambda n: f'user{n}')
+    username = factory.Faker('name')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     email = factory.Faker('email')
     password = factory.Faker('password')
 
 
-class CustomerFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Customer
-
-    user = factory.SubFactory(UserFactory)
-    phone = factory.Faker('phone_number')
-    birth_date = factory.Faker('date_of_birth')
-    membership = factory.Faker('random_element', elements=['B', 'S', 'G'])
-
-
 class AddressFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Address
 
-    customer = factory.SubFactory(CustomerFactory)
     street = factory.Faker('street_address')
     city = factory.Faker('city')
     zip_code = factory.Faker('zipcode')
+
+    @staticmethod
+    def create_user_with_profile_addresses(user_size, address_size=2):
+        customers = UserFactory.create_batch(user_size)
+        [AddressFactory.create_batch(address_size, customer_id=customer.id) for customer in customers]
--- store/management/commands/store_seed.py
+++ store/management/commands/store_seed.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from store import factory
+from store.models import Address
 
 
 class Command(BaseCommand):
@@ -7,5 +8,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.SUCCESS('Seeding database...'))
-        factory.UserFactory.create_batch(2)
+        factory.AddressFactory.create_user_with_profile_addresses(user_size=1)
         self.stdout.write(self.style.SUCCESS('Database seeded successfully'))
