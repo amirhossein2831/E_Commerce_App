7003755a4ec613294d07932b67beb67a01781228
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-31 15:20:41 +0330

    :hammer: chore: update the manager of the Taged item

--- tags/models.py
+++ tags/models.py
@@ -3,11 +3,24 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
 
+class TaggedItemManager(models.Manager):
+
+    @staticmethod
+    def get_tags_for(content_type, object_id):
+        content_type = ContentType.objects.get_for_model(content_type)
+
+        return TaggedItem.objects.select_related('tag').filter(
+            content_type=content_type,
+            object_id=object_id
+        )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
