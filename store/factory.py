c4ce1d5fe74fcb9cd7a1d0555f5b8efb0615abed
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-31 23:00:45 +0330

    :hammer: chore: create adress facotry

--- store/factory.py
+++ store/factory.py
@@ -1,14 +1,14 @@
 import factory
 from django.conf import settings
 
-from store.models import Customer
+from store.models import *
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = settings.AUTH_USER_MODEL
 
-    username = factory.Faker('name')
+    username = factory.Sequence(lambda n: f'user{n}')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     email = factory.Faker('email')
@@ -23,3 +23,13 @@ class CustomerFactory(factory.django.DjangoModelFactory):
     phone = factory.Faker('phone_number')
     birth_date = factory.Faker('date_of_birth')
     membership = factory.Faker('random_element', elements=['B', 'S', 'G'])
+
+
+class AddressFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Address
+
+    customer = factory.SubFactory(CustomerFactory)
+    street = factory.Faker('street_address')
+    city = factory.Faker('city')
+    zip_code = factory.Faker('zipcode')
