5db3cca641405761b2c01f9c603b64cd53002a3b
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-01 00:36:54 +0330

    :hammer: chore: create collection + product + promotion seeder

--- store/factory.py
+++ store/factory.py
@@ -27,3 +27,41 @@ class AddressFactory(factory.django.DjangoModelFactory):
     def create_user_with_profile_addresses(user_size, address_size=2):
         customers = UserFactory.create_batch(user_size)
         [AddressFactory.create_batch(address_size, customer_id=customer.id) for customer in customers]
+
+
+class CollectionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Collection
+
+    title = factory.Faker('sentence', nb_words=4)
+    featured_product = None
+
+
+class PromotionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Promotion
+
+    description = factory.Faker('text')
+    discount = factory.Faker('random_int', min=1, max=50)
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    title = factory.Faker('sentence', nb_words=4)
+    slug = factory.Faker('slug')
+    description = factory.Faker('text')
+    unit_price = factory.Faker('random_int', min=10, max=1000)
+    inventory = factory.Faker('random_int', min=0, max=1000)
+
+    @staticmethod
+    def create_collection_product_promotions(collections_size, product_size=5, promotions_size=2):
+        collections = CollectionFactory.create_batch(collections_size)
+        products_list = [ProductFactory.create_batch(product_size, collection=collection) for collection in collections]
+
+        for products in products_list:
+            for product in products:
+                promotions = PromotionFactory.create_batch(promotions_size)
+                product.promotions.set(promotions)
+                product.save()
--- store/management/commands/store_seed.py
+++ store/management/commands/store_seed.py
@@ -1,6 +1,5 @@
 from django.core.management import BaseCommand
 from store import factory
-from store.models import Address
 
 
 class Command(BaseCommand):
@@ -8,5 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.SUCCESS('Seeding database...'))
-        factory.AddressFactory.create_user_with_profile_addresses(user_size=1)
+        # Run the factory here
+        # factory.AddressFactory.create_user_with_profile_addresses(user_size=1)
+        # factory.ProductFactory.create_collection_product_promotions(collections_size=1)
         self.stdout.write(self.style.SUCCESS('Database seeded successfully'))
