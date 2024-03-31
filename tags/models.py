304c0654ceb07c20e8ccddfe65a3db2694cb2f0c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-31 15:19:53 +0330

    :sparkles: feat: create Tag app and generate the item

---
+++ tags/models.py
@@ -0,0 +1,15 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
