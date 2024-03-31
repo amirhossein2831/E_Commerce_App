03e272f231515155773f35bdf712b4dde42bd828
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-01 01:06:41 +0330

    :hammer: chore: use transation in factory

--- store/management/commands/store_seed.py
+++ store/management/commands/store_seed.py
@@ -1,4 +1,6 @@
 from django.core.management import BaseCommand
+from django.db import transaction
+
 from store import factory
 
 
@@ -6,11 +8,14 @@ class Command(BaseCommand):
     help = 'Seed Store tables with sample data'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('Seeding database...'))
-        # Run the factory here
-        factory.AddressFactory.create_user_with_profile_addresses(user_size=10)
-        factory.ProductFactory.create_collection_product_promotions_reviews(collections_size=10)
-        factory.CartItemFactory.create_cart_cart_items(10)
-        factory.OrderItemFactory.create_order_order_items(10)
+        try:
+            with transaction.atomic():
+                # Run the factory here
+                factory.AddressFactory.create_user_with_profile_addresses(user_size=10)
+                factory.ProductFactory.create_collection_product_promotions_reviews(collections_size=10)
+                factory.CartItemFactory.create_cart_cart_items(10)
+                factory.OrderItemFactory.create_order_order_items(10)
 
-        self.stdout.write(self.style.SUCCESS('Database seeded successfully'))
+                self.stdout.write(self.style.SUCCESS('Database seeded successfully'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Database seeding failed: {e}'))
