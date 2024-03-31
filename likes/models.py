864b05f6f42e07488fdeb5653f6787ccd40411ee
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-31 15:23:43 +0330

    :hammer: chore: update the like model manager

--- likes/models.py
+++ likes/models.py
@@ -5,8 +5,20 @@ from django.db import models
 from core.models import User
 
 
-# Create your models here.
-class LikeItem(models.Model):
+class LikedItemManager(models.Manager):
+
+    @staticmethod
+    def get_like_for(content_type, object_id):
+        content_type = ContentType.objects.get_for_model(content_type)
+
+        return LikedItem.objects.filter(
+            content_type=content_type,
+            object_id=object_id
+        )
+
+
+class LikedItem(models.Model):
+    objects = LikedItemManager()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='likes')
