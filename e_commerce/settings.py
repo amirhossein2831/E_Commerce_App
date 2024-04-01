acc201d333474b7831f5dc8ba4ee6f1f65fb833c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-01 23:52:46 +0330

    :hammer: chore: define the media variable

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
