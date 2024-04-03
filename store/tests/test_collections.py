a4b2e2f0583f921268b8407db291e572a3179c2f
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-04 00:34:52 +0330

    :hammer: chore: use fixture and simplify test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,34 +1,41 @@
+import pytest
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+
+    return do_create_collection
 
 
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401(self):
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'test'})
+    def test_if_user_is_anonymous_return_401(self, api_client, create_collection):
+        response = create_collection({'title': 'test'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_403(self):
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'test'})
+    def test_if_user_is_not_admin_return_403(self, authenticated_client, create_collection):
+        authenticated_client()
+
+        response = create_collection({'title': 'test'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_return_400(self):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+    def test_if_data_is_invalid_return_400(self, authenticated_client, create_collection):
+        authenticated_client(is_staff=True)
+
+        response = create_collection({'title': ''})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_return_200(self, db):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'test'})
+    def test_if_data_is_valid_return_200(self, authenticated_client, create_collection, db):
+        authenticated_client(is_staff=True)
+
+        response = create_collection({'title': 'test'})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
