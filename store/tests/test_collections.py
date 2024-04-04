cbc12caf475c8b60774800f7d5c94f1b54cfb134
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-04 18:24:31 +0330

    :white_check_mark: test: add Query test for collections (reading)

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,19 @@
 import pytest
-from django.contrib.auth.models import User
+from model_bakery import baker
 from rest_framework import status
+
+from store.models import Collection
+
+
+@pytest.fixture
+def get_collections(api_client):
+    def do_get_collections(pk=None):
+        if pk is not None:
+            return api_client.get(f'/store/collections/{pk}/')
+        else:
+            return api_client.get('/store/collections/')
+
+    return do_get_collections
 
 
 @pytest.fixture
@@ -11,7 +24,35 @@ def create_collection(api_client):
     return do_create_collection
 
 
-class TestCreateCollection:
+class TestQueryCollection:
+    def test_if_user_is_anonymous_return_401(self, get_collections):
+        response = get_collections()
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    @pytest.mark.django_db
+    def test_retrieve_collections_list(self, authenticated_client, get_collections):
+        authenticated_client()
+        collections_number = 4
+        baker.make(Collection, _quantity=collections_number)
+
+        response = get_collections()
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == collections_number
+
+    @pytest.mark.django_db
+    def test_retrieve_collections_detail(self, authenticated_client, get_collections):
+        authenticated_client()
+        collection = baker.make(Collection)
+
+        response = get_collections(pk=collection.id)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == collection.id
+
+
+class TestCommandCollection:
     def test_if_user_is_anonymous_return_401(self, api_client, create_collection):
         response = create_collection({'title': 'test'})
 
@@ -32,7 +73,8 @@ class TestCreateCollection:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_return_200(self, authenticated_client, create_collection, db):
+    @pytest.mark.django_db
+    def test_if_data_is_valid_return_200(self, authenticated_client, create_collection):
         authenticated_client(is_staff=True)
 
         response = create_collection({'title': 'test'})
