ba31d59304b29e453f4d9241b1e4e1836d6fdeec
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-04 18:45:52 +0330

    :hammer: chore: add update and delete test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -31,7 +31,7 @@ class TestQueryCollection:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     @pytest.mark.django_db
-    def test_retrieve_collections_list(self, authenticated_client, get_collections):
+    def test_if_user_is_authenticate_return_200_list(self, authenticated_client, get_collections):
         authenticated_client()
         collections_number = 4
         baker.make(Collection, _quantity=collections_number)
@@ -42,7 +42,7 @@ class TestQueryCollection:
         assert response.data['count'] == collections_number
 
     @pytest.mark.django_db
-    def test_retrieve_collections_detail(self, authenticated_client, get_collections):
+    def test_if_user_is_authenticate_return_200_detail(self, authenticated_client, get_collections):
         authenticated_client()
         collection = baker.make(Collection)
 
@@ -65,7 +65,7 @@ class TestCommandCollection:
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_return_400(self, authenticated_client, create_collection):
+    def test_create_if_data_is_invalid_return_400(self, authenticated_client, create_collection):
         authenticated_client(is_staff=True)
 
         response = create_collection({'title': ''})
@@ -74,10 +74,31 @@ class TestCommandCollection:
         assert response.data['title'] is not None
 
     @pytest.mark.django_db
-    def test_if_data_is_valid_return_200(self, authenticated_client, create_collection):
+    def test_create_if_data_is_valid_return_200(self, authenticated_client, create_collection):
         authenticated_client(is_staff=True)
 
         response = create_collection({'title': 'test'})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+    @pytest.mark.django_db
+    def test_update_if_user_is_admin_return_204(self, authenticated_client, api_client):
+        authenticated_client(is_staff=True)
+        updated_title = 'updated'
+        collection = baker.make(Collection)
+
+        response = api_client.patch(f'/store/collections/{collection.id}/', {'title': updated_title})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['title'] == updated_title
+
+    @pytest.mark.django_db
+    def test_delete_if_user_is_admin_return_204(self, authenticated_client, api_client):
+        authenticated_client(is_staff=True)
+        collection = baker.make(Collection)
+
+        response = api_client.delete(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert not Collection.objects.filter(pk=collection.id).exists()
