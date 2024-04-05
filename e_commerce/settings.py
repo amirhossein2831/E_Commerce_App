99429e0bbcb776f6c5c4d7e4c9fc09270b658589
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-05 13:59:23 +0330

    :wrench: conf: add Static root path

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -189,6 +189,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
